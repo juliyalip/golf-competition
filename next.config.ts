6cd6b3edb36af52463aa96fd0e30e4a6e937e34a
Juliya Lipatova <575@ukr.net>
2025-05-13 20:57:38 +0200

    update settings

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   basePath: '/golf-competition',
   assetPrefix: '/golf-competition/',
-   experimental: {
-    appDir: true
-  }
 };
 
 module.exports = nextConfig;
